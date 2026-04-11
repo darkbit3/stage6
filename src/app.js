c25c57b81f825f067e428eedae828d75c2524dce
kaleab_ <kaleabayenew519@gmail.com>
2026-04-11 12:57:32 +0300

    add stage6 withou

--- src/app.js
+++ src/app.js
@@ -491,8 +491,8 @@ async function createNewGameForStage(stage) {
     return {
       gameId: gameId,
       payout: 0,
-      players: [],
-      boards: [],
+      players: '',
+      boards: '',
       totalPlayers: 0,
       stage: stage.toUpperCase(),
       timestamp: new Date().toISOString(),
